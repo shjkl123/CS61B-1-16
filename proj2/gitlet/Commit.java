244f1b14642bbdbf1c991b0f49bcaea5e328714d
sunhang <3783463685@qq.com>
2025-04-19 14:19:28 +0800

    clear the projectt2, do it again

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,23 +2,15 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import java.io.File;
-import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date; // TODO: You'll likely use this in this class
-import java.util.SimpleTimeZone;
-import static gitlet.Utils.*;
+
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
  *  @author TODO
  */
-public class Commit implements Serializable {
+public class Commit {
     /**
      * TODO: add instance variables here.
      *
@@ -29,44 +21,6 @@ public class Commit implements Serializable {
 
     /** The message of this Commit. */
     private String message;
+
     /* TODO: fill in the rest of this class. */
-    private Commit parent;
-    private String timestamp;
-
-    public Commit(String message, Commit parent) {
-        this.message = message;
-        this.parent = parent;
-        if (parent == null) {
-            timestamp = "00:00:00 UTC, Thursday, 1 January 1970";
-        } else {
-            ZonedDateTime utcTime = ZonedDateTime.now(ZoneOffset.UTC);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss 'UTC' EEEE, d MMMM yyyy");
-            timestamp = utcTime.format(formatter);
-        }
-
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public Commit getParent() {
-        return parent;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void saveCommit() {
-        File file = Utils.join(Repository.GITLET_COMMIT, sha1(this));
-        writeObject(file, this);
-    }
-
-    public static Commit getCommit(String sha1) {
-        File file = Utils.join(Repository.GITLET_COMMIT, sha1);
-        return readObject(file, Commit.class);
-    }
-
-
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,9 +1,6 @@
 package gitlet;
 
-import javax.swing.*;
 import java.io.File;
-import java.io.IOException;
-
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -26,72 +23,7 @@ public class Repository {
     /** The current working directory. */
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
-    public static final File GITLET_COMMIT = join(CWD, "commit");
     public static final File GITLET_DIR = join(CWD, ".gitlet");
-    public static final File GITLET_LOG = join(GITLET_DIR, "log");
-    public static final File GITLET_ADDITION_STAGE = join(GITLET_DIR, "additionStage");
-    public static final File GITLET_Blobs = join(GITLET_DIR, "blobs");
-    public static final File GITLET_REF = join(GITLET_DIR, "ref");
+
     /* TODO: fill in the rest of this class. */
-
-
-    public static void setupPersistence() {
-        if (!GITLET_LOG.exists()) GITLET_LOG.mkdir();
-        if (!GITLET_ADDITION_STAGE.exists()) GITLET_ADDITION_STAGE.mkdir();
-        if (!GITLET_Blobs.exists()) GITLET_Blobs.mkdir();
-        if (!GITLET_REF.exists()) GITLET_REF.mkdir();
-    }
-
-    public static void createGitletFile() {
-        if (!GITLET_DIR.exists()) GITLET_DIR.mkdir();
-    }
-
-    public static void setMasterPointer(String sha1) {
-        File masterFile = join(GITLET_REF, "master.txt");
-        writeContents(masterFile, sha1);
-    }
-
-    public static void setHeadPointer(String sha1) {
-        File headFile = join(GITLET_REF, "head.txt");
-        writeContents(headFile, sha1);
-    }
-
-    public static String getMaster() {
-        File masterFile = join(GITLET_REF, "master.txt");
-        return readContentsAsString(masterFile);
-    }
-
-    public static String getHead() {
-        File headFile = join(GITLET_REF, "head.txt");
-        return readContentsAsString(headFile);
-    }
-
-
-    public static void addAdditionStage(File file) {
-        Blob b = new Blob(file);
-        b.saveBlob(GITLET_ADDITION_STAGE);
-    }
-
-    public static void removeAdditionState() {
-        Repository.GITLET_ADDITION_STAGE.delete();
-        if (!Repository.GITLET_ADDITION_STAGE.exists())
-            Repository.GITLET_ADDITION_STAGE.mkdir();
-    }
-
-    public static void createLogFile() {
-        if (!GITLET_LOG.exists()) {
-            GITLET_LOG.mkdir();
-        }
-    }
-
-    public static void addLog(Commit commit) {
-        String s = "===\n";
-        String sha1 = sha1(commit);
-        String data = commit.getTimestamp();
-        String message = commit.getMessage();
-        s += "commit " + sha1 + "\nData: " + data + "\n" + message;
-        File file = join(GITLET_LOG, sha1 + ".txt");
-
-
-    }
 }
